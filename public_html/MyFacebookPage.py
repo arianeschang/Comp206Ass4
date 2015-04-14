544c69436ef8408e21b13145a6ade487339d0e27
arianeschang <arianeschang@gmail.com>
2015-04-13 22:09:08 -0400

    new

--- public_html/MyFacebookPage.py
+++ public_html/MyFacebookPage.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 import sys
 import cgi
-user_name = "aaa"
+
+
+user_name=""
 form = cgi.FieldStorage()
 
 def find_friends(user):
@@ -11,7 +13,6 @@ def find_friends(user):
     for line in ref:
         lst.append(line)
     ref.close()
-
     for x in range(0, len(lst)):
         lst[x] = lst[x].strip()
         lst[x] = lst[x].split(" ")
@@ -20,15 +21,13 @@ def find_friends(user):
                 friend_lst.append(lst[x][y])
     return friend_lst
 
-
-def is_friends(user1, user2):
-    if user1 in find_friends(user2):
+def is_friends(me, him):
+    if him in find_friends(me):
         return True
-    elif user1==user2:
+    elif me==him:
         return True
     else:
         return False
-
 
 def all_users():
     ref = open("members.csv", "r")
@@ -51,7 +50,6 @@ def recent_posts(user):
     for line in ref:
         lst.append(line)
     ref.close()
-
     for x in range(0, len(lst)):
         if (x % 2 == 0):
             name_lst.append(lst[x].strip())
@@ -67,15 +65,16 @@ def recent_posts(user):
         c = (a, b)
         if is_friends(a, user):
             lst.insert(x, c)
-
+    lst=lst[0:10]
     return lst
 
 def posts_html(lst):
-    for i in range(0, len(lst)):
+    if lst==[]:
+      string=""
+    else:
+      for i in range(0, len(lst)):
         lst[i]="</p> <blockquote>".join(lst[i])
-
-    string = "<p>" + "</blockquote> <hr /> <p>".join(lst) + "</blockquote>"
-
+        string = "<p>" + "</blockquote> <hr /> <p>".join(lst) + "</blockquote>"
     return string
 
 def is_member(user):
@@ -101,67 +100,109 @@ def add_friend(user1, user2):
     ref = open ("members.csv","w")
     ref.write(lst)
     ref.close()
-
     return 0
 
 message="""
-<html style="font-family:Arial,Helvetica; sans-serif; color:#333">
-<head>
-  <title>HTML Layout using Tables</title>
-</head>
-<body style="background:#ccc; margin=0">
- <div id="container" style="width:960px; margin:0 auto; background:#999;">
-   <div id="header" style="width:100%; height:80px; background:#333">
-      <div id="logo" style="float:left; width:60px; height:60px; margin:10px; background:#ccc">logo</div>
-      <div id="title" style="float:left; width:120px; height:60px; background:#666; margin:10px"><h1>MovMe</h1></div>
-      <div id="top info" style="float:right; width:100px; height:60px; background:#666; margin:10px">top info</div>
 
-      <div id="navbar" style="clear:both; height:20px; float:right">
-        <ul style="margin:0; list-style-type:none; padding:0" >
-           <li style="float:left; padding:5px">
-              Welcome {2}!
-           </li>
-           <li style="float:left; padding:5px;">
-              <a href="http://www.cs.mcgill.ca/~aschan" style="font-size:15px; float:left; display:block;" >
-                LogOut
-              </a>
-          </li>
-        </ul>
-      </div>
-   </div>
-   <div id="content area" style="width:700px; float:left; clear:both; margin:10px">
-      <div id="banner" style="background:#666; height:70px; width:700px; clear:both; padding:10px">
-         Share your ideas!
-         <form action="MyFacebookPage.py" method="POST">
-             Add Comments:<br />
-             <input type="hidden" name="user" value="{5}">
-             <input type="text" name="comment">
-             <input type="submit" value="submit">
-         </form>
-      </div>
-      <div id="left col" style="color:#fff; margin:10px; background:#000;" align="center">
+<html>
+
+<head>
+<title>MovMe</title>
+</head>
+<body bgcolor="ccffcc">
+<table border="1px">
+  <tr>
+    <th rowspan="2">
+      <img src="http://www.lemonchutney.com/wp-content/uploads/2013/04/10-greatest-movie-posters-ever-made-04-420-75.jpg" alt="Silence of the Lambs" width="100px">
+
+      <img src="http://main-designyoutrust.netdna-ssl.com/wp-content/uploads/2012/11/titanic-movie-poster-1997-1020339699.jpg" alt="Titanic" width="100px">
+
+      <br>
+      <img src="http://www.movieposter.com/posters/archive/main/9/A70-4902" alt="Matrix" width="100px">
+      
+      <img src="http://www.freedesign4.me/wp-content/gallery/posters/free-movie-film-poster-harry-potter-phoenix.jpg" alt="HarryPotter" width="100px">
+
+      <br>
+      <img src="http://netdna.webdesignerdepot.com/uploads/2011/02/jurassicpark.jpg" alt="Jurassic Park" width="100px">
+      
+      <img src="http://images2.fanpop.com/images/photos/8400000/Movie-Posters-movies-8405245-1224-1773.jpg" alt="POC" width="100px">
+
+      <br>
+      <img src="http://www.kitaro10.com/wp-content/uploads/2010/03/film-poster-51.jpg" alt="Salt" width="100px">
+      
+      <img src="http://meetinthelobby.com/wp-content/uploads/2012/01/Jennifer_Lawrence_The_Hunger_Games_Movie_Poster.jpg" alt="The Hunger Games" width="100px">
+
+    </th>
+    <th colspan="2" align="center" valign="top" width="75%" height="150px">
+      <font face="Abadi MT Condensed Light" size="8">
+        MovMe  
+      </font>
+
+      <br>
+      <img src=" https://scontent-lga.xx.fbcdn.net/hphotos-xaf1/v/t1.0-9/11102865_10153208606604483_3697769982336368912_n.jpg?oh=7477219f53b32e00a69df221a02c1048&oe=55E56669" alt="titlePic" width="90px">
+      <br> 
+      Welcome {2}
+      <a href="http://www.cs.mcgill.ca/~aschan/welcome.html">
+      LogOut
+      </a>
+    </th>
+    <th rowspan="2">
+      <img src="http://www.lemonchutney.com/wp-content/uploads/2013/04/10-greatest-movie-posters-ever-made-04-420-75.jpg" alt="Silence of the Lambs" width="100px">
+
+      <img src="http://main-designyoutrust.netdna-ssl.com/wp-content/uploads/2012/11/titanic-movie-poster-1997-1020339699.jpg" alt="Titanic" width="100px">
+
+      <br>
+      <img src="http://www.movieposter.com/posters/archive/main/9/A70-4902" alt="Matrix" width="100px">
+      
+      <img src="http://www.freedesign4.me/wp-content/gallery/posters/free-movie-film-poster-harry-potter-phoenix.jpg" alt="HarryPotter" width="100px">
+
+      <br>
+      <img src="http://netdna.webdesignerdepot.com/uploads/2011/02/jurassicpark.jpg" alt="Jurassic Park" width="100px">
+      
+      <img src="http://images2.fanpop.com/images/photos/8400000/Movie-Posters-movies-8405245-1224-1773.jpg" alt="POC" width="100px">
+
+      <br>
+      <img src="http://www.kitaro10.com/wp-content/uploads/2010/03/film-poster-51.jpg" alt="Salt" width="100px">
+      
+      <img src="http://meetinthelobby.com/wp-content/uploads/2012/01/Jennifer_Lawrence_The_Hunger_Games_Movie_Poster.jpg" alt="The Hunger Games" width="100px">
+  </th>
+  </tr>
+  <tr>
+    <td align="center" valign="top" width="25%"><b>Share your ideas!</b>
+      <form action="MyFacebookPage.py" method="POST">
+        <input type="hidden" name="user" value="{4}">
+          Add Comments:<br />
+        <input type="text" name="comment" size="80">
+        <input type="submit" value="submit"> <br /><br/>
+
          <iframe width="560" height="315" src="https://www.youtube.com/embed/yQ5U8suTUw0" frameborder="0" allowfullscreen>
          </iframe>
-      </div>
-   </div>
-   <div id="right banner" style="float:right; width:150px; margin:10px">
-      <div id="Members" style="flaot:left; background:#888; margin:0 0 5px 0; padding:5px"><b>Members</b>{1}</div>
-      <div id="Friends" style="flaot:left; background:#888; clear:both; margin:0 0 5px 0; padding:5px"><b>Friends</b>{3}</div>
-      <div id="add" style="flaot:left; background:#888; clear:both;padding:5px"><b>Add Friends</b>
+         <br/><br/>
+          <b>Recent Comments...</b>
+          {0}
+      </form>
+    </td>
+    <td align="center" valign="top" width="15%">
+      <div id="right banner" width="150px">
+        <div id="Members"><b>Members</b><br />{1}</div><br/>
+        <div id="Friends"><b>Friends</b><br />{3}</div><br/>
+        <div id="add"><b>Add Friends<br />
         <form action="MyFacebookPage.py" method="POST">
-           <input type="hidden" name="user" value="{4}">
-           <input type="text" name="friend" size="12"><br />
-           <input type="submit" value="Add">
-         </form>
+          <input type="hidden" name="user" value="{5}">
+          <input type="text" name="friend" size="12"><br />
+          <input type="submit" value="Add">
+        </form>
+       </div>
       </div>
-   </div>
-   <div id="recent_comments" style="padding:20px; background:#888; width:700px; margin:10px; float:left">
-      recent_comments
-      {0}
-   </div>
- </div>
+    </td>
+  </tr>
+</table>
 </body>
 </html>"""
+
+if form.has_key('user'):
+    user = form['user'].value.strip()
+    user_name = user
 
 if form.has_key('comment'):
     comment = form['comment'].value.strip()
@@ -172,13 +213,10 @@ if form.has_key('comment'):
     ref.write(user_name+"\n")
     ref.write(comment+"\n")
     ref.close()
+
 if form.has_key('friend'):
     friend = form['friend'].value.strip()
     y=add_friend(user_name, friend)
-if form.has_key('user'):
-    user = form['user'].value.strip()
-    user_name = user
-
 
 post = posts_html(recent_posts(user_name))
 members = all_users()
